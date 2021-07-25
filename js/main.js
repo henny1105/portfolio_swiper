78af8f997c17b41e9753ecc4c154a0fc7fe8ffe5
kimhye1105 <kimhyein1105@gmail.com>
2021-07-26 02:13:02 +0900

    스와이퍼 포트폴리오 추가

---
+++ js/main.js
@@ -0,0 +1,80 @@
+const menu = ["Tomorrowland", "Star Wars", "Adventureland", "Paradise Gardens Park"];
+
+const swiper = new Swiper("#wrap", {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 50,
+    mousewheel: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+        renderBullet: function(index, className){
+            return `<span class="${className}">${menu[index]}</span>`
+        }
+
+    },
+    /*
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 400,
+        slideShadows: false
+    }
+    */
+});
+
+//Dom Caching
+const bgs = document.querySelectorAll(".bg li");
+const prev = document.querySelector(".swiper-button-prev");
+const next = document.querySelector(".swiper-button-next");
+const navi = document.querySelectorAll(".swiper-pagination span");
+
+next.addEventListener("click", activation);
+prev.addEventListener("click", activation);
+window.addEventListener("mousewheel", activation);
+swiper.on("slideChangeTransitionEnd", activation);
+
+for(let el of navi){
+    el.addEventListener("click", e=>{
+        const isOn = e.currentTarget.classList.contains("swiper-pagination-bullet-active");
+        if(isOn) return;
+        swiper.on("slideChangeTransitionEnd", activation);
+    })
+}
+
+
+function activation(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+
+    for(let el of bgs){
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+}
+
+
+// video play
+$(".pause").on("click", function(){
+    $(".swiper-slide-active").find("video").get(0).pause();
+ });
+ 
+ $(".play").on("click", function(){
+    $(".swiper-slide-active").find("video").get(0).play();
+    $(".swiper-slide-active").find("video").css({
+       filter: "saturate(100%)"
+    });
+ });
+ 
+ $(".load").on("click", function(){
+    $(".swiper-slide-active").find("video").get(0).load();
+ });
